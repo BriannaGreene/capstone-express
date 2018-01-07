4f9c8ff1376f2be1800339f1f9e5426732c10366
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-07 12:09:11 -0700

    first migration created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var tickets = require('./routes/tickets')
+var groups = require('./routes/groups')
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/tickets', tickets)
+app.use('/groups', groups)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/groups.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next) => {
+  // code goes here
+  knex('table_name')
+    .select('*')
+    .orderBy('id')
+    .then(data => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(data))
+    })
+    .catch((err) => next(err))
+})
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  // code goes here
+})
+
+router.post('/', (req, res, next) => {
+  const { item } = req.body
+  // code goes here
+})
+
+router.patch('/:id', (req, res, next) => {
+  const id = req.params.id
+  const { item } = req.body
+  // code goes here
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  // code goes here
+})
+
+module.exports = router;
